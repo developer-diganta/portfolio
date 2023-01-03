e67d39b021ae638dba9c6c3da2c2d275a813da84
Diganta Kr Banik <digantabanik2000@gmail.com>
2023-01-04 01:22:13 +0530

    tech

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,14 @@ module.exports = {
       indigo: colors.indigo,
       purple: colors.purple,
       pink: colors.pink, 
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      trueGray: colors.trueGray,
+      warmGray: colors.warmGray,
+      coolGray: colors.coolGray,
+      blueGray: colors.blueGray,
+      
     },
     fontFamily: {
       'spartan': ['"League Spartan"', 'sans-serif'],
